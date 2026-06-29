b7add066e52bfa852c1045bc982087d5c3370572
Daniel Palomino <dpalomip@nttdata.com>
2026-06-29 18:03:34 -0500

    agregar watch target

---
+++ HeartWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartWatch Watch App
+//
+//  Created by Daniel Eduardo Palomino Pacahuala on 29/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartWatch Watch App/HeartWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartWatchApp.swift
+//  HeartWatch Watch App
+//
+//  Created by Daniel Eduardo Palomino Pacahuala on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct HeartWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
